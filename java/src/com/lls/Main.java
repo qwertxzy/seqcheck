6c63eb0397957de12b24ea9d3bc9b0389847e34e
qwertxzy <leo@qwertxzy.me>
2021-06-01 18:52:13 +0200

    Adding main logic for parsing arguments and calling model parser & checker methods

--- java/src/com/lls/Main.java
+++ java/src/com/lls/Main.java
@@ -1,8 +1,42 @@
 package com.lls;
 
-public class Main {
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+public class Main {
+    private static final String RESET = "\033[0m";  // Text Reset
+    private static final String RED = "\033[0;31m";     // Red console color
+    private static final String GREEN = "\033[0;32m";   // Green console color
+
+    // Takes one argument:
+    //      - Path to a directory of BLIF files or one BLIF file
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            if (args.length != 1) {
+                throw new Exception("Please provide one argument.");
+            }
+
+            File blifFile = new File(args[0]);
+
+            if (blifFile.isDirectory()) {
+                throw new Exception("Please specify only one BLIF file, it can contain several modules");
+            }
+
+            // Read the BLIF file as one big string and pass it to the model parser
+            String blifInput = Files.readString(Path.of(args[0]));
+            ModelParser parser = new ModelParser(blifInput);
+
+            // Loop over all the parsed models and determine whether they are sequential or not
+            for (BlifModel m : parser.getParsedBlif()){
+                System.out.println("Model " + m.name + " is " + (ModelChecker.checkModel(m) ? RED + "sequential" + RESET :  GREEN + "combinational" + RESET));
+            }
+        } catch (Exception e) {
+            System.out.println("Error while running program, please invoke with");
+            System.out.println("java -jar Program.jar <Path to generated BLIF file>");
+
+            System.out.println("Error message: " + e.getMessage());
+            System.exit(1);
+        }
     }
 }
