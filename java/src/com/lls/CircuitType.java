dfe5562a591129fbba414572966e7b9900246ec1
qwertxzy <leo@qwertxzy.me>
2021-06-04 15:50:30 +0200

    Fixing typo in CircuitType.java

--- java/src/com/lls/CircuitType.java
+++ java/src/com/lls/CircuitType.java
@@ -5,10 +5,10 @@ public enum CircuitType {
   SEQUENTIAL,
   LOOPING;
 
-  private static final String RESET = "\033[0m";      // Text Reset
-  private static final String RED = "\033[0;31m";     // Red console color
-  private static final String GREEN = "\033[0;32m";   // Green console color
-  public static final String YELLOW = "\033[0;33m";   // Yellow console color
+  private static final String RESET   = "\033[0m";      // Text Reset
+  private static final String RED     = "\033[0;31m";   // Red console color
+  private static final String GREEN   = "\033[0;32m";   // Green console color
+  private static final String YELLOW  = "\033[0;33m";   // Yellow console color
 
   // Pretty printing
   @Override
