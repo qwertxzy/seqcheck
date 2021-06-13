5dec874c2b2b08829a53992991e136f5942a62fd
Kubicki, Jonas <jonas.kubicki@stud.tu-darmstadt.de>
2021-06-13 09:08:14 +0200

    Removed colored output

--- java/src/com/lls/Main.java
+++ java/src/com/lls/Main.java
@@ -2,6 +2,7 @@ package com.lls;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
